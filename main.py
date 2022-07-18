7acdd9f77950c9dcce132c4e0357e21d59684fa3
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-07-18 17:14:00 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+
+gender = "male"
+weight_kg = "70"
+height_cm = "1.70"
+AGE = "33"
+add_row_sheety_endpoint = "your sheety add row endpoint"
+nutrition_app_id = "app id from nutrition page"
+nutrition_api_key = "api key from nutrition page"
+exercise_endpoint = "exercice page endpoint"
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {"x-app-id": nutrition_app_id,
+           "x-app-key": nutrition_api_key,
+           }
+parameters = {
+    "query": exercise_text,
+    "gender": gender,
+    "weight_kg": weight_kg,
+    "height_cm": height_cm,
+    "age": AGE
+}
+
+response_nutrition = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
+result = response_nutrition.json()
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+headers_sheety = {"Authorization": "Bearer lexandru"}
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+
+        }
+    }
+
+    sheet_response = requests.post(add_row_sheety_endpoint, json=sheet_inputs, headers=headers_sheety)
+
+    print(sheet_response.text)
